eb7d78609e2ef914cef69e9cd7891cacbb6b3710
ashoo12 <88502853+ashoo12@users.noreply.github.com>
2024-02-21 12:37:31 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+
+
+morse_code_dict = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    '0': '-----',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.'
+}
+user_text=input("Enter your text here and i will convert it into morse code.\n").upper()
+morse_code=''
+for letter in user_text:
+    for character in morse_code_dict:
+        code=morse_code_dict[character]
+        if letter == character:
+           morse_code+= code+','
+
+
+print(f'Morse code for your text is {morse_code}')
+
